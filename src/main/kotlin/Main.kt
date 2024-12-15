c7581464c0034af7234afd4df07e75bc0515116a
admltfr <adamlutfiramadhan@gmail.com>
2024-12-16 00:22:26 +0700

    Added main process

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,96 +1,85 @@
-//const tidak dapat berada didalam method/fungsi hingga dideklarasikan diluarnya
 
-const val pi = 3.14
-enum class Direction {
-    east,
-    west,
-    north,
-    south
+class Total{
+    var refbalance : Int = 0
+
+    constructor(refbalance : Int) {
+        this.refbalance = refbalance
+    }
+    fun withdraw(amount : Int ) {
+        if (refbalance-amount >= 0) {
+            refbalance -=  amount
+            println("Balance after withdraw : $refbalance")
+        } else {
+            println("Insufficient Balance")
+        }
+    }
+    fun deposit(amount : Int) {
+        refbalance += amount
+        println("Balance after deposit : $refbalance")
+    }
 }
+
 fun main() {
-    //val name = "Kotlin"
-    var name = "Kotlin"
-    name = "Java"
-    var i = 0
-    //keyword val, nama variabel
-    //tipe data val tidak bisa diassign ulang sedangkan var bisa
-    //val termasuk tipe dinamis
+    val balance = Total(10)
+    var programExit : Boolean = false
+    while (!programExit) {
+        menu()
+        val option: String = readln()
+        optionValidator(arrayOf("0","1","2","3"),option)
+        when (option) {
+            "0" -> {
+                programExit = true
+            }
 
-    println(pi)
-    println(name)
+            "1"-> {
+                println("Currently your balance is ${balance.refbalance}")
+            }
 
-//    if (name == "Java") {
-//        name = "Kotlin"
-//    } else {
-//        name = "Java Bukan Kotlin"
-//    }
-//    println(name)
+            "2" -> {
+                print("Enter Deposit Amount : ")
+                val depositamount: Int = readln().toInt()
+                balance.deposit(depositamount)
+            }
 
-    //versi lain dari if di kotlin adalah when
+            "3" -> {
+                print("Enter Withdraw Amount : ")
+                val withdrawamount: Int = readln().toInt()
+                balance.withdraw(withdrawamount)
+            }
 
-    when (name) {
-        "Java" -> println("Kotlin Bukan Java")
-        "Kotlin" -> println("Java Bukan Kotlin")
-        else -> println("Salah")
+            else -> {
+                println("Invalid Option, please enter the right options")
+            }
+        }
+        print("Continue? [Y/N] : ")
+        if (!programExit) {
+            val confirmExit : String = readln()
+            optionValidator(arrayOf("Y","N"),confirmExit)
+            if (confirmExit == "N") {
+                programExit = true
+            }
+        }
+
     }
-//when ibaratnya sebagai switch case di bahasa lain
-    //jangan pakai when jika operatornya seperti > atai < karena pasti dia ditulis 1 1 angkanya
-    //1 -> nama
-    //2 -> nama
-    //... -> nama
-    //WHEN LEBIH CEPAT DARI IF
-    //karena when sendiri dia langsung search nilainya
-    //sedangkan if di tes 1 1
-    for (i in 1..2){
-        println("Kotlin")
-    }
-    //PELAJARI FOREACH
-    //enum
-    val direction_var = Direction.east
-    //enum dideklarasikan diluar method
-    //enum merupakan suatu set atribut yang tidak bisa diubah lagi
-    when (direction_var) {
-        Direction.east -> println("Timur")
-        Direction.west -> println("Barat")
-        Direction.north -> println("Utara")
-        Direction.south -> println("Selatan")
-        else -> println("Gak kemana mana")
-    }
-
-    //lambda function
-    //atau anonymous function
-    val x = 10
-    val y = 10
-    val hasil = process(x) {
-        x -> {y*2}
-    }
-    println(hasil)
-
-
 }
 
-//fun process(a:Int, lambda: (a:Int) -> Int) : Int {
-//    return a * lambda(a)
-//}
-
-//fun process(a:Int, kalidua2 : (a:Int) -> Int) : Int {
-//    return a * kalidua2(a)
-//}
-
-fun process(a:Int, kalidua2 : (a:Int) -> (a:Int) -> Int) : Int {
-    return a * kalidua2(a)(a)
+fun optionValidator(optionList : Array<String>, optionInput : String) {
+    if (optionList.contains(optionInput)) {
+        println("Processing request...")
+    } else {
+        while (!optionList.contains(optionInput)) {
+            print("Invalid Option, please enter the right options : ")
+            val optionInput = readln()
+        }
+    }
 }
 
 
-//nama nim jurusan
-//buat proyek misalnya kalkulator
-//untuk ubah integer ke string tingal .tostring
-//jika misal string ke integer .toint
-//lambda bisa berbagai nama contohnya kalidua
-
-//contoh lambda yaitu onclick
-//fun main() {
-//    button("text") {
-//        tampilkatalog = true
-//    }
-//}
+fun menu() {
+    println("==========Menu==========")
+    println("1. Check Balance        ")
+    println("2. Deposit Balance      ")
+    println("3. Withdraw Balance     ")
+    println("0. Exit                 ")
+    print("Enter Option : ")
+}
